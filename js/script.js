c1da9df7d9b96dd2ed0e8981d6d0038e33aabc05
Nowack <svetlganina@yandex.ru>
2017-09-11 21:07:24 +0300

    js формат

--- js/script.js
+++ js/script.js
@@ -1,26 +1,18 @@
 
-  var link = document.querySelector(".feedback-link");
-  var popup = document.querySelector(".feedback-modal");
-  var close = document.querySelector(".modal-close");
+var link = document.querySelector(".feedback-link");
+var popup = document.querySelector(".feedback-modal");
+var close = document.querySelector(".modal-close");
 
-  link.addEventListener("click", function (evt){
-    evt.preventDefault();
-    popup.classList.add("modal-show");
-    }
-  );
-
-  close.addEventListener("click", function (evt){
-    evt.preventDefault();
+link.addEventListener("click", function(evt){
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+});
+close.addEventListener("click", function(evt){
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+});
+window.addEventListener("keydown", function(evt){
+  if (evt.keyCode === 27 && popup.classList.contains("modal-show")){
     popup.classList.remove("modal-show");
-    }
-  );
-
-
-
-   window.addEventListener("keydown", function (evt) {
-     if (evt.keyCode === 27) {
-       if (popup.classList.contains("modal-show")) {
-         popup.classList.remove("modal-show");
-       }
-     }
-   });
+  }
+});
